b636fa6872654f73886790f84e707ac0831de667
vfavorito <vince.favorito@gmail.com>
2021-02-26 22:42:05 -0600

    added portraits to table component

--- employee-directory/src/components/Table/table.js
+++ employee-directory/src/components/Table/table.js
@@ -7,14 +7,16 @@ function Table(props) {
         <table className="table">
             <thead>
                 <tr className="tableHead">
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Phone Number</th>
+                    <th>Portrait:</th>
+                    <th>First Name:</th>
+                    <th>Last Name:</th>
+                    <th>Phone Number:</th>
                 </tr>
             </thead>
             <tbody>
                 {props.employees.map(employee =>
                     <tr key={employee.id.value}>
+                        <td><img alt="portrait" src={employee.picture.thumbnail}></img></td>
                         <td>{employee.name.first}</td>
                         <td>{employee.name.last}</td>
                         <td>{employee.phone}</td>
